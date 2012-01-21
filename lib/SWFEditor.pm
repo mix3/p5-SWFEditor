fa50263236260fb8d5052f0c51c24a46beab87a8
mix3 <himachocost333@hotmail.co.jp>
2012-01-22 06:50:25 +0900

    remove Exporter

--- lib/SWFEditor.pm
+++ lib/SWFEditor.pm
@@ -4,27 +4,6 @@ use 5.008;
 use strict;
 use warnings;
 
-require Exporter;
-
-our @ISA = qw(Exporter);
-
-# Items to export into callers namespace by default. Note: do not export
-# names by default without a very good reason. Use EXPORT_OK instead.
-# Do not simply export all your public functions/methods/constants.
-
-# This allows declaration	use SWFEditor ':all';
-# If you do not need this, moving things directly into @EXPORT or @EXPORT_OK
-# will save memory.
-our %EXPORT_TAGS = ( 'all' => [ qw(
-	
-) ] );
-
-our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
-
-our @EXPORT = qw(
-	
-);
-
 our $VERSION = '0.01';
 
 require XSLoader;
@@ -44,10 +23,10 @@ sub DESTORY {
     _swf_object_close($self);
 }
 
-my $SHAPE_BITMAP_NONE           = 0;
-my $SHAPE_BITMAP_MATRIX_RESCALE = 1;
-my $SHAPE_BITMAP_RECT_RESIZE    = 2;
-my $SHAPE_BITMAP_TYPE_TILLED    = 4;
+our $SHAPE_BITMAP_NONE           = 0;
+our $SHAPE_BITMAP_MATRIX_RESCALE = 1;
+our $SHAPE_BITMAP_RECT_RESIZE    = 2;
+our $SHAPE_BITMAP_TYPE_TILLED    = 4;
 
 sub input {
     my $self = shift;
